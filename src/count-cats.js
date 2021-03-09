a856ca63f61ca4b28a54804d9bc32064e8f7e002
KirillMalko <malkokirill099@gmail.com>
2021-03-09 22:31:08 +0300

    fix: create fail

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,10 +1,24 @@
-const CustomError = require("../extensions/custom-error");
+/*const CustomError = require("../extensions/custom-error");
 
 module.exports = function countCats( matrix ) {
-  throw new CustomError('Not implemented');
   let array = [];
   for (let i = 0; i < matrix.length; i++) {
-    matrix[i].map(event => (e === '^^') && array.push(event))
+    matrix[i].map(event => (event === '^^') && array.push(event))
   }
   return array.length
+};
+*/
+
+const CustomError = require("../extensions/custom-error");
+
+module.exports = function countCats(matrix) {
+  let res = []
+
+  for(let i = 0; i < matrix.length; i++) {
+    for(let j = 0; j < matrix[i].length; j++) {
+      res.push(matrix[i][j])
+    }
+  }
+  res = res.filter((event) => event === '^^')
+  return res.length
 };
