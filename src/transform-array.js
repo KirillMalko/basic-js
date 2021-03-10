601844126aecba0b21c1043cfbcbbaa008ae3878
KirillMalko <malkokirill099@gmail.com>
2021-03-11 01:12:22 +0300

    added task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,39 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error();
+  }
+  let array = arr.split();
+  for(let i = 0; i<arr.length; i++){
+    if (array[i] === '--double-next') {
+      if(i !== array.length - 1){
+        array[i] = array[i+1];
+      }
+    }
+    if (array[i] === '--double-prev') {
+      if (i !== 0) {
+        array[i] = array[i - 1];
+        i++;
+      }
+    }
+    if (array[i] === '--discard-next') {
+      if (i !== array.length - 1) {
+        array[i + 1] = array[i];
+        i++;
+      }
+    }
+    if (array[i] === '--discard-prev') {
+      if (i !== 0) {
+        array[i - 1] = array[i];
+      }
+    }
+  }
+  return array.filter(
+      (element) =>
+          element !== '--discard-next' &&
+          element !== '--discard-prev' &&
+          element !== '--double-next' &&
+          element !== '--double-prev'
+  );
 };
